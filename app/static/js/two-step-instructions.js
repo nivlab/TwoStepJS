628167f62774d100b44dd9b94110039f3fed530f
J Branson Byers <jbransonbyers@gmail.com>
2020-03-31 15:41:00 -0400

    Added instructions and block breaks. Added multiple trials.

---
+++ app/static/js/two-step-instructions.js
@@ -0,0 +1,5 @@
+var instructions = ["Instructions will go here."];
+
+var block_end = ["You have made it to the end of the block. Press any key when you are ready to continue."];
+
+var exp_end = ["You have reached the end of the experiment. Press any key to continue."]
