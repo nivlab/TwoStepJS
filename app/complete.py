8ad64841d0283741b8ae5d70bad70bdf56e333b8
J Branson Byers <jbransonbyers@gmail.com>
2020-08-10 17:32:11 -0400

    moved files into niturk 1.0 architecture

--- app/complete.py
+++ app/complete.py
@@ -1,28 +1,37 @@
+import os, configparser
 from flask import (Blueprint, redirect, render_template, request, session, url_for)
 from .io import write_metadata
 
 ## Initialize blueprint.
 bp = Blueprint('complete', __name__)
 
+## Define root directory.
+ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+## Load and parse configuration file.
+cfg = configparser.ConfigParser()
+cfg.read(os.path.join(ROOT_DIR, 'app.ini'))
+
+## Specify completion URLs (real and decoy).
+url_stem = "https://app.prolific.co/submissions/complete?cc="
+complete_url = url_stem + cfg['FLASK']['COMPLETION_CODE']
+decoy_url = url_stem + cfg['FLASK']['DECOY_CODE']
+
 @bp.route('/complete')
 def complete():
     """Present completion screen to participant."""
 
-    ## Error-catching: screen for previous visits.
-    if 'complete' in session:
+    ## Case 1: navigation to completion page without completion flag
+    if 'complete' not in session or session['complete'] == False:
 
         ## Update participant metadata.
-        session['WARNING'] = "Revisited complete page."
-        write_metadata(session, ['WARNING'], 'a')
+        session['ERROR'] = "1012: Visited completion page without valid completion flag."
+        write_metadata(session, ['ERROR'], 'a')
+        return redirect(decoy_url)
 
+    ## Case 2: data_pass
     else:
 
         ## Update participant metadata.
-        session['complete'] = True
         write_metadata(session, ['complete'], 'a')
-
-    ## DEV NOTE:
-    ## If you want a custom completion code, replace the return statement with:
-    ## > render_template('complete.html', value=session['complete'])
-
-    return render_template('complete.html')
+        return redirect(complete_url)
